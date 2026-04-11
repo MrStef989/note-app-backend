0c4468c6da8be684802bb954c1eccb197bed2d45
Denis Gitelman <den.gitelman@gmail.com>
2026-04-11 15:12:05 +0300

    Note-App Sprint#1

--- src/test/java/ru/atc/university/noteapp/NoteappApplicationTests.java
+++ src/test/java/ru/atc/university/noteapp/NoteappApplicationTests.java
@@ -1,9 +1,10 @@
 package ru.atc.university.noteapp;
 
+import com.yaobezyana.YaObezYanaApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = YaObezYanaApplication.class)
 class NoteappApplicationTests {
 
     @Test
